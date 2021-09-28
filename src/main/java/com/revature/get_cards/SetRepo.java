d758afc218c598d935cd89ea84e5495b1b82f0ff
kimatec <kimtranatec14@gmail.com>
2021-09-28 15:46:24 -0500

    added set & setRepo to get setId

---
+++ src/main/java/com/revature/get_cards/SetRepo.java
@@ -0,0 +1,32 @@
+package com.revature.get_cards;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SetRepo {
+
+    private final DynamoDBMapper dbReader;
+
+    public SetRepo(){ dbReader = new DynamoDBMapper(AmazonDynamoDBClientBuilder.defaultClient()); }
+
+    public Set getSetId(String id){
+
+        Map<String, AttributeValue> input = new HashMap<>();
+        input.put(":id", new AttributeValue().withS(id));
+
+        DynamoDBScanExpression query = new DynamoDBScanExpression()
+                .withFilterExpression("id = :id")
+                .withExpressionAttributeValues(input);
+
+        List<Set> result = dbReader.scan(Set.class, query);
+
+        return result.get(0);
+    }
+}
